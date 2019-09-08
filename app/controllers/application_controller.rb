45dbddc084aa8c19b2f0afffca091a9dcfc4fdf1
kevin lang <langkevina@gmail.com>
2019-09-08 15:32:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,12 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+# index action
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
-
+# show action
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
