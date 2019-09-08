6d94aac240f4fe27cf4b0e4c1f60adceadbbd1c8
kevin lang <langkevina@gmail.com>
2019-09-08 15:43:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,48 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-# index action
-  get '/recipes' do
+  # index
+  get "/recipes" do
     @recipes = Recipe.all
     erb :index
   end
-# show action
-  get '/recipes/:id' do
-    @recipe = Recipe.find_by_id(params[:id])
+
+  # new
+  get "/recipes/new" do
+    @recipe = Recipe.new
+    erb :new
+  end
+
+  # create
+  post "/recipes" do
+    @recipe = Recipe.create(params)
+    redirect to "/articles/#{ @recipe.id }"
+  end
+
+  # show
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
     erb :show
   end
+
+  # edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  # update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #destroy
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+
 end
