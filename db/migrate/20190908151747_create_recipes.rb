5b258dc021d5df1ab2f71e455cc93aa2ccc5f83d
kevin lang <langkevina@gmail.com>
2019-09-08 15:19:18 +0000

    Automatically backed up by Learn

--- db/migrate/20190908151747_create_recipes.rb
+++ db/migrate/20190908151747_create_recipes.rb
@@ -1,4 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.integer :ingredients
+    end
+
   end
 end
